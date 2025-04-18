8f2d5d44ce134610c80fbc586feab4f9c7260b64
Zavorotna <zavorotnaa96@gmail.com>
2025-04-18 13:28:13 +0300

    subcategory crud

--- resources/views/components/succes.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="successPopup" id="successPopup"><div class="cancel_popup"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
-<path d="M1 21L20.5816 1M21 21L1.41844 1" stroke="#121212" stroke-width="2" stroke-linecap="round"/>
-</svg> </a></div><div> <a href="index.html"><img src="img/logo.svg" alt=""/></a></div><h2>дякуємо за замовлення!</h2><p>Ваше замовлення прийняте, і ми працюємо над його обробкою.</p><div class="cta_conatiner black_cta_popup"><a class="cta catalog_cta" href="">на головну</a></div></div>
